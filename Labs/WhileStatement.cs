de22897cbcaf642be66b5083b9625a2004615777
Deniqtrius Curry <Deniqtrius.curry@outlook.com>
2018-03-06 05:11:47 -0800

    Commit

---
+++ Labs/WhileStatement.cs
@@ -0,0 +1,8 @@
+﻿1>------ Build started: Project: WhileStatement, Configuration: Debug x86 ------
+1>  WhileStatement -> C:\Users\Deniqtrius\Desktop\lzma1801\Microsoft Press\VCSBS\Chapter 5\WhileStatement\WhileStatement\bin\x86\Debug\WhileStatement.exe
+2>------ Deploy started: Project: WhileStatement, Configuration: Debug x86 ------
+2>Updating the layout...
+2>Copying files: Total <1 mb to layout...
+2>Deployment complete (0:00:00.216). Full package name: "80b3f23c-29f4-4229-849a-7a16fb6303b0_1.0.0.0_x86__k96968rvhjvje"
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Deploy: 1 succeeded, 0 failed, 0 skipped ==========
